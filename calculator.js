5e734d2b9cbdb04b0eea14521a805a096a5ecfcf
Vlad Saraev <vlad@hotmail.co.nz>
2019-10-08 14:22:42 +1300

    es6 arrow functions

--- calculator.js
+++ calculator.js
@@ -100,21 +100,10 @@ function operatorHandler(operator) {
   }
 }
 
-function add(a, b) {
-  return a + b;
-}
-
-function subtract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  return a / b;
-}
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
 
 function operate(operator, a, b) {
   switch(operator) {
